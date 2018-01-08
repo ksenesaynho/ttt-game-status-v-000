629c834495925a08c35aeddd07a2cd7469b8e62f
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-08 20:46:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,28 +17,46 @@ WIN_COMBINATIONS = [
   
   # Define won?, full?, draw?, over?, and winner below
   
-  def won?(board)
-    WIN_COMBINATIONS.dectect do |combo|
-      board[combo[0]] == board[combo[1]] &&
-      board[combo[1]] == board[combo[2]] &&
-      position_taken?(board, combo[0])
-    end
-  end
-  
-  def full?(board)
-    board.all?{|token| token == "X" || token == "0"}
-  end
-  
-  def draw?(board)
-    full?(board) && !won?(board)
-  end
-  
-  def over?(board)
-    won?(board) || full?(board)
-  end
-  
-  def winner?(board)
-    if winning_combo = won?(board)
-      board[winning_combo.first]
-    end
-  end
+ 7 WIN_COMBINATIONS = [ 
+8   [0,1,2], 
+9   [3,4,5], 
+10   [6,7,8], 
+11   [0,3,6], 
+12   [1,4,7], 
+13   [2,5,8], 
+14   [0,4,8], 
+15   [6,4,2] 
+16 ] 
+17 
+ 
+18 # Define won?, full?, draw?, over?, and winner below 
+19 def won?(board) 
+20   WIN_COMBINATIONS.detect do |combo| 
+21     board[combo[0]] == board[combo[1]] && 
+22     board[combo[1]] == board[combo[2]] && 
+23     position_taken?(board, combo[0]) 
+24   end 
+25 end 
+26 
+ 
+27 def full?(board) 
+28   board.all?{|token| token == "X" || token == "O"} 
+29 end 
+30 
+ 
+31 def draw?(board) 
+32   full?(board) && !won?(board) 
+33 end 
+34 
+ 
+35 def over?(board) 
+36   won?(board) || full?(board) 
+37 end 
+38 
+ 
+39 def winner(board) 
+40   if winning_combo = won?(board) 
+41     board[winning_combo.first] 
+42   end 
+43 end 
+
